c3ab87721078837f6fe6e84fd2e6a8cec4ea78d7
may <m4rch3n1ng@gmail.com>
2023-09-29 13:58:50 +0200

    replace lazy_static with once_cell::Lazy

--- src/style.rs
+++ src/style.rs
@@ -1,11 +1,8 @@
 //! Style utility
 use is_unicode_supported::is_unicode_supported;
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 
-lazy_static! {
-	/// Does the terminal support unicode
-	pub static ref IS_UNICODE: bool = is_unicode_supported();
-}
+pub static IS_UNICODE: Lazy<bool> = Lazy::new(is_unicode_supported);
 
 fn is_unicode(unicode: &'static str, non_unicode: &'static str) -> &'static str {
 	if *IS_UNICODE {
@@ -20,34 +17,32 @@ fn is_unicode(unicode: &'static str, non_unicode: &'static str) -> &'static str 
 /// Changes if the terminal supports unicode.
 pub mod chars {
 	use super::is_unicode;
-	use lazy_static::lazy_static;
+	use once_cell::sync::Lazy;
 
-	lazy_static! {
-		/// Straight left bar
-		pub static ref BAR: &'static str = is_unicode("│", "|");
-		/// Start bar
-		pub static ref BAR_START: &'static str = is_unicode("┌", "T");
-		/// End bar
-		pub static ref BAR_END: &'static str = is_unicode("└", "—");
-		/// Active step
-		pub static ref STEP_ACTIVE: &'static str = is_unicode("◆", "*");
-		/// Cancelled step
-		pub static ref STEP_CANCEL: &'static str = is_unicode("■", "x");
-		/// Error step
-		pub static ref STEP_ERROR: &'static str = is_unicode("▲", "x");
-		/// Submitted step
-		pub static ref STEP_SUBMIT: &'static str = is_unicode("◇", "o");
-		/// Active radio
-		pub static ref RADIO_ACTIVE: &'static str = is_unicode("●", ">");
-		/// Inactive radio
-		pub static ref RADIO_INACTIVE: &'static str = is_unicode("○", " ");
-		/// Active checkbox
-		pub static ref CHECKBOX_ACTIVE: &'static str = is_unicode("◻", "[.]");
-		/// Selected checkbox
-		pub static ref CHECKBOX_SELECTED: &'static str = is_unicode("◼", "[+]");
-		/// Inactive checkbox
-		pub static ref CHECKBOX_INACTIVE: &'static str = is_unicode("◻", "[ ]");
-	}
+	/// Straight left bar
+	pub static BAR: Lazy<&str> = Lazy::new(|| is_unicode("│", "|"));
+	/// Start bar
+	pub static BAR_START: Lazy<&str> = Lazy::new(|| is_unicode("┌", "T"));
+	/// End bar
+	pub static BAR_END: Lazy<&str> = Lazy::new(|| is_unicode("└", "—"));
+	/// Active step
+	pub static STEP_ACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("◆", "*"));
+	/// Cancelled step
+	pub static STEP_CANCEL: Lazy<&str> = Lazy::new(|| is_unicode("■", "x"));
+	/// Error step
+	pub static STEP_ERROR: Lazy<&str> = Lazy::new(|| is_unicode("▲", "x"));
+	/// Submitted step
+	pub static STEP_SUBMIT: Lazy<&str> = Lazy::new(|| is_unicode("◇", "o"));
+	/// Active radio
+	pub static RADIO_ACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("●", ">"));
+	/// Inactive radio
+	pub static RADIO_INACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("○", " "));
+	/// Active checkbox
+	pub static CHECKBOX_ACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("◻", "[.]"));
+	/// Selected checkbox
+	pub static CHECKBOX_SELECTED: Lazy<&str> = Lazy::new(|| is_unicode("◼", "[+]"));
+	/// Inactive checkbox
+	pub static CHECKBOX_INACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("◻", "[ ]"));
 }
 
 /// ANSI escape codes
