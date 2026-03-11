da8a4a1119a484ba0409c168119d95b0ae55f12c
may <m4rch3n1ng@gmail.com>
2026-03-11 23:05:38 +0100

    remove direct dependency on once_cell
    still in through a transitive dependency but its fine

--- src/style.rs
+++ src/style.rs
@@ -1,9 +1,9 @@
 //! Style utility
 
 use is_unicode_supported::is_unicode_supported;
-use once_cell::sync::Lazy;
+use std::sync::LazyLock;
 
-pub(crate) static IS_UNICODE: Lazy<bool> = Lazy::new(is_unicode_supported);
+pub(crate) static IS_UNICODE: LazyLock<bool> = LazyLock::new(is_unicode_supported);
 
 fn is_unicode(unicode: &'static str, non_unicode: &'static str) -> &'static str {
 	if *IS_UNICODE {
@@ -18,32 +18,32 @@ fn is_unicode(unicode: &'static str, non_unicode: &'static str) -> &'static str 
 /// Changes if the terminal supports unicode.
 pub mod chars {
 	use super::is_unicode;
-	use once_cell::sync::Lazy;
+	use std::sync::LazyLock;
 
 	/// Straight left bar
-	pub static BAR: Lazy<&str> = Lazy::new(|| is_unicode("│", "|"));
+	pub static BAR: LazyLock<&str> = LazyLock::new(|| is_unicode("│", "|"));
 	/// Start bar
-	pub static BAR_START: Lazy<&str> = Lazy::new(|| is_unicode("┌", "T"));
+	pub static BAR_START: LazyLock<&str> = LazyLock::new(|| is_unicode("┌", "T"));
 	/// End bar
-	pub static BAR_END: Lazy<&str> = Lazy::new(|| is_unicode("└", "—"));
+	pub static BAR_END: LazyLock<&str> = LazyLock::new(|| is_unicode("└", "—"));
 	/// Active step
-	pub static STEP_ACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("◆", "*"));
+	pub static STEP_ACTIVE: LazyLock<&str> = LazyLock::new(|| is_unicode("◆", "*"));
 	/// Cancelled step
-	pub static STEP_CANCEL: Lazy<&str> = Lazy::new(|| is_unicode("■", "x"));
+	pub static STEP_CANCEL: LazyLock<&str> = LazyLock::new(|| is_unicode("■", "x"));
 	/// Error step
-	pub static STEP_ERROR: Lazy<&str> = Lazy::new(|| is_unicode("▲", "x"));
+	pub static STEP_ERROR: LazyLock<&str> = LazyLock::new(|| is_unicode("▲", "x"));
 	/// Submitted step
-	pub static STEP_SUBMIT: Lazy<&str> = Lazy::new(|| is_unicode("◇", "o"));
+	pub static STEP_SUBMIT: LazyLock<&str> = LazyLock::new(|| is_unicode("◇", "o"));
 	/// Active radio
-	pub static RADIO_ACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("●", ">"));
+	pub static RADIO_ACTIVE: LazyLock<&str> = LazyLock::new(|| is_unicode("●", ">"));
 	/// Inactive radio
-	pub static RADIO_INACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("○", " "));
+	pub static RADIO_INACTIVE: LazyLock<&str> = LazyLock::new(|| is_unicode("○", " "));
 	/// Active checkbox
-	pub static CHECKBOX_ACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("◻", "[.]"));
+	pub static CHECKBOX_ACTIVE: LazyLock<&str> = LazyLock::new(|| is_unicode("◻", "[.]"));
 	/// Selected checkbox
-	pub static CHECKBOX_SELECTED: Lazy<&str> = Lazy::new(|| is_unicode("◼", "[+]"));
+	pub static CHECKBOX_SELECTED: LazyLock<&str> = LazyLock::new(|| is_unicode("◼", "[+]"));
 	/// Inactive checkbox
-	pub static CHECKBOX_INACTIVE: Lazy<&str> = Lazy::new(|| is_unicode("◻", "[ ]"));
+	pub static CHECKBOX_INACTIVE: LazyLock<&str> = LazyLock::new(|| is_unicode("◻", "[ ]"));
 }
 
 /// ANSI escape codes
